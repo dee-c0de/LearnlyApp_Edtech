9d14d375a15e649213db1cd081f19ab57762e390
dee-c0de <104325456+dee-c0de@users.noreply.github.com>
2024-08-09 00:47:36 +0100

    Create app.vue
    Front end code contained in source - src/app.vue

---
+++ app.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">E-commerce</a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+          </li>
+          <li class="nav-item" v-if="isAuthenticated">
+            <router-link to="/add-product" class="nav-link">Add Product</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item" v-if="!isAuthenticated">
+            <router-link to="/login" class="nav-link">Login</router-link>
+          </li>
+          <li class="nav-item" v-if="isAuthenticated">
+            <a href="#" class="nav-link" @click="logout">Logout</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  computed: {
+    ...mapGetters(['isAuthenticated']),
+  },
+  methods: {
+    ...mapActions(['logout']),
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+}
+
+.navbar {
+  margin-bottom: 20px;
+}
+</style>
