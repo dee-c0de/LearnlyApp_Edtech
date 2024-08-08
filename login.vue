5b2364efa38f02e15e8f887bf3196d5e4244a6a9
dee-c0de <104325456+dee-c0de@users.noreply.github.com>
2024-08-09 00:15:07 +0100

    Create login.vue
    Front end code contained in source - src/component/login.vue

---
+++ login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <form @submit.prevent="submitForm">
+      <div>
+        <label for="email">Email:</label>
+        <input type="email" v-model="email" required>
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" required>
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    ...mapActions(['login']),
+    submitForm() {
+      this.login({ email: this.email, password: this.password })
+        .then(() => {
+          this.$router.push('/');
+        });
+    }
+  }
+};
+</script>
+
+<style>
+form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+}
+
+form div {
+  display: flex;
+  flex-direction: column;
+}
+
+button {
+  padding: 10px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+</style>
